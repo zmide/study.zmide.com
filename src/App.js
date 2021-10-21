6a666054736338712883c060495ff63b2349c67d
PBK-Bin <bin@PBK6.cn>
2021-10-21 18:54:40 +0800

    feat: 完成首页简单布局

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Button, InputGroup, Input } from 'rsuite';
+import { Search } from '@rsuite/icons';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [keyword, setkeyword] = useState('');
+
+	return (
+		<div className="container">
+			<header className="header">
+				<h1 className="logo">全能搜题</h1>
+			</header>
+			<div className="context">
+				<div className="search_box">
+					<InputGroup size="lg">
+						<Input placeholder={'让我看看你遇到什么样的难题了。'} onChange={(value) => setkeyword(value)} />
+						<InputGroup.Button
+							onClick={() => {
+								console.log('点击了');
+							}}
+						>
+							<Search />
+						</InputGroup.Button>
+					</InputGroup>
+				</div>
+			</div>
+			<div className="footer">
+				<p>@逐梦工作室 提供技术支持</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import 'rsuite/dist/rsuite.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +15,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
